6fc4d33a70162c75b59d804374cc12e44a6188ac
Andrew <coolmac@list.ru>
2025-01-06 10:21:11 +0000

    Initial commit

---
+++ src/Trait/GenerateSlug.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace CoolmacJedi\Traits;
+
+use Illuminate\Support\Str;
+
+trait GeneratesSlug
+{
+    /**
+     * Генерация уникального slug.
+     *
+     * @param string $value Значение для генерации slug
+     * @param string $column Название столбца slug в модели
+     * @param int|null $id Исключение записи по ID (например, для обновления)
+     * @return string Уникальный slug
+     */
+    public static function generateUniqueSlug(string $value, string $column = 'slug', ?int $id = null): string
+    {
+        $slug = Str::slug($value);
+        $originalSlug = $slug;
+
+        $exists = static::where($column, $slug)
+            ->when($id, fn($query) => $query->where('id', '!=', $id))
+            ->exists();
+
+        $counter = 1;
+
+        while ($exists) {
+            $slug = "{$originalSlug}-{$counter}";
+
+            $exists = static::where($column, $slug)
+                ->when($id, fn($query) => $query->where('id', '!=', $id))
+                ->exists();
+
+            $counter++;
+        }
+
+        return $slug;
+    }
+}
